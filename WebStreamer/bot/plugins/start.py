f20cb88ca22aa175eab3d37cbfb6eda2db8b5a48
Khoya Sitara <99271761+khoyaSitara@users.noreply.github.com>
2023-10-25 11:56:18 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -12,9 +12,9 @@ async def start(_, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply(
             "You are not in the allowed list of users who can use me. \
-            Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.",
+            Check <a href='https://telegram.me/eliteflix_official'>this link</a> for more info.",
             disable_web_page_preview=True, quote=True
         )
     await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant download link.'
     )
